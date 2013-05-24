9149760d9dd62d359bf9f544da00a16e2b984e89
Nick Sutterer <apotonick@gmail.com>
2013-05-24 15:52:27 +1000

    don't add falsey return value to results.

--- lib/hooks/hook.rb
+++ lib/hooks/hook.rb
@@ -36,10 +36,9 @@ module Hooks
     def run(scope, *args)
       inject(Results.new) do |results, callback|
         executed = execute_callback(scope, callback, *args)
-        results << executed
 
         return results.halted! unless continue_execution?(executed)
-        results
+        results << executed
       end
     end
 
