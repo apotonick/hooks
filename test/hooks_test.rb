d786548d1418a6fc3948a0c51fa88d75d6985888
Nick Sutterer <apotonick@gmail.com>
2013-11-06 16:23:37 +1100

    move instance tests to InstanceHooksTest.

--- test/hooks_test.rb
+++ test/hooks_test.rb
@@ -58,48 +58,6 @@ class HooksTest < MiniTest::Spec
       # TODO: check if options are not shared!
     end
 
-    # TODO: move to InstanceHooksTest or something.
-    describe "#define_hook" do
-      let(:klass) { Class.new(TestClass) do
-        include Hooks::InstanceHooks
-      end }
-
-      subject { klass.new }
-
-      it "adds hook to instance" do
-        subject.define_hook :after_eight
-
-        assert_equal [], subject.callbacks_for_hook(:after_eight)
-      end
-
-      it "copies existing class hook" do
-        klass.define_hook :after_eight
-        klass.after_eight :dine
-
-        assert_equal [:dine], subject.callbacks_for_hook(:after_eight)
-      end
-
-      describe "#after_eight (adding callbacks)" do
-        before do
-          subject.define_hook :after_eight
-          subject.after_eight :dine
-        end
-
-        it "adds #after_eight hook" do
-          assert_equal [:dine], subject.callbacks_for_hook(:after_eight)
-        end
-
-        it "responds to #run_hook" do
-          subject.instance_eval do
-            def dine; executed << :dine; end
-          end
-
-          subject.run_hook :after_eight
-          subject.executed.must_equal [:dine]
-        end
-      end
-    end
-
     describe "Hooks#run_hook" do
       it "run without parameters" do
         subject.instance_eval do
